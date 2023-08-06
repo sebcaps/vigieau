713391b7b25c5e5ddad8a92bfdf4a2b5f2f3eeac
sebcc <sebastien.carecolin@gmail.com>
2023-08-06 17:32:12 +0000

    add Thematique in file

--- custom_components/vigieau/tests/generate_list.py
+++ custom_components/vigieau/tests/generate_list.py
@@ -7,42 +7,54 @@ from pathlib import Path
 
 BASE_URL = "https://api.vigieau.beta.gouv.fr"
 GEOAPI_GOUV_URL = "https://geo.api.gouv.fr/communes?"
+
+
 async def get_insee_list():
     session = aiohttp.ClientSession()
-    resp =  await session.get(GEOAPI_GOUV_URL)
+    resp = await session.get(GEOAPI_GOUV_URL)
 
-    if  resp.status != 200:
-        print (f"Error Calling GEOAPI with code {resp.status}")
+    if resp.status != 200:
+        print(f"Error Calling GEOAPI with code {resp.status}")
         return {}
 
     return await resp.json()
 
-async def get_restriction( city_code:str):
+
+async def get_restriction(city_code: str):
     url = f"{BASE_URL}/reglementation?&commune={city_code}&profil=particulier"
     session = aiohttp.ClientSession()
-    resp =  await session.get(url)
+    resp = await session.get(url)
 
-    if  resp.status != 200:
+    if resp.status != 200:
         return {}
 
     return await resp.json()
 
+
 async def main():
-    restriction_list={"usages":[]}
+    restriction_list = {"restrictions": []}
+    usage_added = []
     commune_list = await get_insee_list()
     for commune in commune_list:
-        # if len(restriction_list["usages"])>= 10:
+        # if len(restriction_list["restrictions"]) >= 10:
         #     break
-        restriction =  await get_restriction(commune["code"])
-        if restriction: #Sometimes insee is enouhg to call vigieau Api, sometimes not exclude the one where it's not enough , for the moment
+        restriction = await get_restriction(commune["code"])
+        if (
+            restriction
+        ):  # Sometimes insee is enouhg to call vigieau Api, sometimes not exclude the one where it's not enough , for the moment
             for usage in restriction.get("usages"):
-                if usage["usage"] not in restriction_list["usages"]:
-                    restriction_list["usages"].append(usage["usage"])
+                if usage["usage"] not in usage_added:
+                    restriction_list["restrictions"].append(
+                        {"thematique": usage["thematique"], "usage": usage["usage"]}
+                    )
+                    usage_added.append(usage["usage"])
 
-    finaldata= json.dumps(restriction_list,ensure_ascii=False) #https://stackoverflow.com/questions/35582528/python-encoding-and-json-dumps
+    finaldata = json.dumps(
+        restriction_list, ensure_ascii=False
+    )  # https://stackoverflow.com/questions/35582528/python-encoding-and-json-dumps
     file = Path("/workspaces/vigieau/custom_components/vigieau/tests/newfile.json")
 
-    with open(file,"w" ,encoding="utf-8") as outfile:
+    with open(file, "w", encoding="utf-8") as outfile:
         outfile.write(finaldata)
 
 
--- custom_components/vigieau/tests/test_regexp.py
+++ custom_components/vigieau/tests/test_regexp.py
@@ -8,23 +8,38 @@ from custom_components.vigieau.const import SENSOR_DEFINITIONS
 
 class Test(unittest.TestCase):
     def test_matcher(self):
-
         # FIXME handle relative path
         # Open file with as many usage as possible ;-)
-        file = Path("/workspaces/vigieau/custom_components/vigieau/tests/usage_list.json")
+        file = Path(
+            "/workspaces/vigieau/custom_components/vigieau/tests/usage_list.json"
+        )
         with open(file) as f:
             input = f.read()
         data = json.loads(input)
 
-        for usage in data['usages']: #For all usages in the list
-            print (usage)
-            with self.subTest(msg='One matcher failed'): # For soft fail, ref https://stackoverflow.com/questions/4732827/continuing-in-pythons-unittest-when-an-assertion-fails
+        for restriction in data["restrictions"]:  # For all restrictions in the list
+            print(restriction)
+            print(restriction["usage"])
+            with self.subTest(
+                msg="One matcher failed"
+            ):  # For soft fail, ref https://stackoverflow.com/questions/4732827/continuing-in-pythons-unittest-when-an-assertion-fails
                 found = False
-                for sensor in SENSOR_DEFINITIONS: # We may have to create a function rather than copy/paste, but it's a 'simple re.search....
+                for (
+                    sensor
+                ) in (
+                    SENSOR_DEFINITIONS
+                ):  # We may have to create a function rather than copy/paste, but it's a 'simple re.search....
                     for matcher in sensor.matchers:
-                        if re.search(matcher,usage,re.IGNORECASE,):
+                        if re.search(
+                            matcher,
+                            restriction["usage"],
+                            re.IGNORECASE,
+                        ):
                             found = True
-                self.assertTrue(found,f"Value {usage} not found in matcher") #Check for one usage if it has been found
+                self.assertTrue(
+                    found,
+                    f"Value **{restriction['usage']}** in category **{restriction['thematique']}** not found in matcher",
+                )  # Check for one usage if it has been found
 
 
 if __name__ == "__main__":
